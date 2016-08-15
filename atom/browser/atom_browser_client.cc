f587b218294fae5e51a49ca85db88fef73dbe4cc
Thiago de Arruda <tpadilha84@gmail.com>
2016-08-15 08:01:24 -0300

    Expose `--enable-sandbox` command-line switch.
    When `--enable-sandbox` is passed, electron will use chromium sandbox to spawn
    renderers, and every new BrowserWindow will behave as if the `sandboxed` option
    was passed to its webPreferences.

--- atom/browser/atom_browser_client.cc
+++ atom/browser/atom_browser_client.cc
@@ -197,6 +197,7 @@ void AtomBrowserClient::AppendExtraCommandLineSwitches(
   // Copy following switches to child process.
   static const char* const kCommonSwitchNames[] = {
     switches::kStandardSchemes,
+    switches::kEnableSandbox
   };
   command_line->CopySwitchesFrom(
       *base::CommandLine::ForCurrentProcess(),
